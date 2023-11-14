e5045046875e13f1d2bd03cba64f9c799f5c7a1e
Kenichi Yamaguchi <k-yamaguchi@gleamorb.co.jp>
2023-11-14 22:52:33 +0900

    レイアウト調整

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,50 +1,46 @@
 $(function () {
   const swiper = new Swiper(".swiper", {
     autoplay: {
-      delay: 3000,
+      delay: 5000,
     },
     navigation: {
-      nextEl: ".swiper-button-next", //「次へボタン」要素指定
-      prevEl: ".swiper-button-prev", //「前へボタン」要素指定
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
-      //使いたいパラメータを追加する
     },
     scrollbar: {
-      el: ".swiper-scrollbar", //要素指定
+      el: ".swiper-scrollbar",
     },
   });
-  /* slick */
-  // $(".slider").slick({
-  //   arrows: false,
-  //   autoplay: true,
-  //   adaptiveHeight: true,
-  //   dots: false,
-  // });
 
-  $(window).on("load", function () {
-    contResizing();
+  $(".header-toggle").click(function () {
+    $(".header-toggle").addClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu").addClass("active");
   });
 
-  $(window).on("resize", function () {
-    if ($(window).width() >= 640) {
-      if ($(window).height() < 990) {
-        contResizing();
+  $(".menu-toggle").click(function () {
+    $(".menu-toggle").addClass("active");
+    $(".header-toggle").removeClass("active");
+    $(".menu").toggleClass("active");
+  });
+
+  function fade() {
+    $(".js-fade").each(function () {
+      var elemPos = $(this).offset().top - 50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("fadeUp");
+      } else {
+        $(this).removeClass("fadeUp");
       }
-    }
-  });
-
-  function contResizing() {
-    const W = 1423;
-    const H = 2500;
-    const contH = $(".sec-message").height() - 100;
-    const contW = $(".sec-message").width() - 100;
-    const rH = contH / H;
-    const rW = contW / W;
-    console.log(contH, contW, rH, rW);
-
-    // const sec_cont_img01 = 432 * rH;
-    // $("#sec-message .img01").css({ width: "auto", height: sec_cont_img01 + "px" });
+    });
   }
+
+  $(window).scroll(function () {
+    fade();
+  });
 });
